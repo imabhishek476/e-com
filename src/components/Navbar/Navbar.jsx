92f6325b06fea3356745b8b87d640e91fd3443fd
Abhishek Gupta <guptaa476.ag@gmail.com>
2024-07-09 14:14:13 +0530

    09-6 update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,17 +12,17 @@ function Navbar() {
     <div className="container sticky top-0 bg-white text-black z-10 p-3 pr-0">
       <div className="flex justify-between items-center">
         <div className="flex flex-col">
-          <span className="font-semibold">Top Fashion Brands</span>
-          <span className="text-sm">Family wear collection</span>
+          <span className="font-semibold text-inherit">Top Fashion Brands</span>
+          <span className="text-xs">Family wear collection</span>
         </div>
-        <div className="relative">
-          <CiSearch className="absolute left-3 top-3 text-gray-600" />
+        <div className="relative sm:w-full sm:max-w-[400px]">
+          <CiSearch className="absolute top-0 left-0 mt-3 ml-5 text-gray-600" />
           <input
             type="search"
             placeholder="Search product"
-            className="pl-10 p-2 text-sm text-gray-600 bg-gray-100 rounded-xl w-[30vw] h-[7vh]"
+            className="bg-[#f2f3f5] border-none outline-none px-12 py-2 rounded-xl w-full"
           />
-          <VscSettings className="absolute right-3 top-3 text-gray-600" />
+          <VscSettings className="absolute top-0 right-0 mt-3 mr-5 text-gray-600"/>
         </div>
         <div className="flex justify-center items-center">
             <div className=" bg-gray-400 rounded-md text-2xl h-[40px] mr-5 cursor-pointer">
