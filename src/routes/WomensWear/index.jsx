434ea1f305ff8464577d0e9fd6225a1a75516b49
Abhishek Gupta <guptaa476.ag@gmail.com>
2024-07-16 22:42:05 +0530

    ui bug fix

--- src/routes/WomensWear/index.jsx
+++ src/routes/WomensWear/index.jsx
@@ -61,12 +61,12 @@ function WomenWithShopingBags() {
     <div className="flex container my-5">
       <img className="w-3/6" src={Women} alt="" />
       <div className="flex flex-col gap-10 justify-around">
-        <div className="flex flex-col gap-3 items-center m-4">
-          <span className="text-4xl text-center">UP TO 50% OFF</span>
-          <span className="text-2xl text-center">
+        <div className="flex flex-col gap-3 items-center m-4 container">
+          <span className="text-4xl  text-gray-500 text-center">UP TO 50% OFF</span>
+          <span className="text-3xl text-center text-secondary font-semibold">
             Unleash Your Style Potential with Our Spectacular Clothing Sale
           </span>
-          <span className="text-lg text-center">
+          <span className="text-lg text-center text-gray-500 font-semibold">
             Feel free to customize these titles to fit the tone and theme of
             your clothing sale and to convey the excitement of the discounts
             being offered.
